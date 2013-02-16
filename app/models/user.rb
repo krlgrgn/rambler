14a8d7ad3f85c2c549a2bb08dd3c6b4d86534744
Karl Grogan <krlgrgn@gmail.com>
2013-02-16 17:53:55 +0000

    Update the User model with password validations and encryption.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :about, :city, :country, :email,
-                  :first_name, :last_name, :state
+                  :first_name, :last_name, :state,
+                  :password, :password_confirmation
 
   # Validations
   validates :email,      presence: true, uniqueness: true,
@@ -8,11 +9,18 @@ class User < ActiveRecord::Base
                                    with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/,
                                    message: "Invalid emaill address."
                          }
-  validates :first_name, presence: true
-  validates :last_name,  presence: true
-  validates :city,       presence: true
-  validates :state,      presence: true
-  validates :country,    presence: true
-  validates :about,      presence: false, allow_blank: true
+  validates :first_name,            presence: true
+  validates :last_name,             presence: true
+  validates :city,                  presence: true
+  validates :state,                 presence: true
+  validates :country,               presence: true
+  validates :about,                 presence: false, allow_blank: true
+  validates :password,              presence: true,  length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
+  # Filters
+  before_save { |user| user.email = email.downcase }
+
+  # methods
+  has_secure_password
 end
