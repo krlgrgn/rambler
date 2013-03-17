17d857ac8511596e1336a24b1bf8d50855ccd731
Karl Grogan <krlgrgn@gmail.com>
2013-03-17 18:35:57 +0000

    Add show and new to the signed_in filter so that only users who have a an account can view or create an adventure

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,5 +1,5 @@
 class AdventuresController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:index, :show, :edit, :new, :update, :destroy]
   before_filter :correct_user, only: [:edit, :update, :destroy]
 
   # GET /adventures
