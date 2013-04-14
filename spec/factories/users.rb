e9e6c8338100d53a6132e62fd77e28f5bae36f70
Karl Grogan <krlgrgn@gmail.com>
2013-04-14 15:26:35 +0100

    Update the user factory with the new user attributes.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,8 @@ FactoryGirl.define do
   factory :user do |f|
     PASSWORD = "password"
     f.email                    { Faker::Internet.email }
+    f.uid                      { rand(10 ** 10) }
+    f.provider                 { Faker::Company.name }
     f.first_name               { Faker::Name.first_name }
     f.last_name                { Faker::Name.last_name }
     f.city                     { Faker::Address.city }
@@ -12,6 +14,7 @@ FactoryGirl.define do
     f.about                    { Faker::Lorem.sentence }
     f.password                 { PASSWORD }
     f.password_confirmation    { PASSWORD }
+    f.image                    { "https://www.google.ie/images/srpr/logo4w.png" }
 
     factory :admin do
       admin true
