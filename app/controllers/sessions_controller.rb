0fd45b140a69a4a7d1b5f96d4c89fd6cf4c981b3
Karl Grogan <krlgrgn@gmail.com>
2013-04-20 16:48:17 +0100

    User respond_to in sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,24 +5,31 @@ class SessionsController < ApplicationController
 
   # POST /sessions
   def create
-    user = User.find_by_email(params[:session][:email])
-    if user and user.authenticate(params[:session][:password])
-      # Sign in the user.
-      sign_in user
-      redirect_back_or user
-    else
-      render 'new'
+    @user = User.find_by_email(params[:session][:email])
+    respond_to do |format|
+      if @user and @user.authenticate(params[:session][:password])
+        # Sign in the user.
+        sign_in(@user)
+        format.html { redirect_back_or @user }
+        format.json { render json: @user }
+      else
+        format.html { render action: "new" }
+      end
     end
   end
 
+  # GET /auth/:provider/callback
   def fb_create
-    user = User.from_omniauth(env["omniauth.auth"])
-    if user
-      # Sign in the user.
-      sign_in user
-      redirect_back_or user
-    else
-      render 'new'
+    @user = User.from_omniauth(env["omniauth.auth"])
+    respond_to do |format|
+      if @user
+        # Sign in the user.
+        sign_in @user
+        format.html { redirect_back_or @user }
+        format.json { render json: @user }
+      else
+        format.html { render action: "new" }
+      end
     end
   end
 
