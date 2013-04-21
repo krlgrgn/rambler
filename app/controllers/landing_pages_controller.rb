c8dd062b2c43396da1cc8761558b479a06b5d178
Karl Grogan <krlgrgn@gmail.com>
2013-04-21 11:27:24 +0100

    Add landing page functionality and views.

---
+++ app/controllers/landing_pages_controller.rb
@@ -0,0 +1,28 @@
+class LandingPagesController < ApplicationController
+  # GET /landing_pages/new
+  # GET /landing_pages/new.json
+  def new
+    @landing_page = LandingPage.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @landing_page }
+    end
+  end
+
+  # POST /landing_pages
+  # POST /landing_pages.json
+  def create
+    @landing_page = LandingPage.new(params[:landing_page])
+
+    respond_to do |format|
+      if @landing_page.save
+        format.html { redirect_to root_path, notice: 'Thank you for expressing your interest.' }
+        format.json { render json: root_path, status: :created, location: root_path }
+      else
+        format.html { render "new" }
+        format.json { render json: root_path, status: :unprocessable_entity }
+      end
+    end
+  end
+end
---
+++ app/models/landing_page.rb
@@ -0,0 +1,15 @@
+class LandingPage < ActiveRecord::Base
+  attr_accessible :email
+
+  EMAIL_REGEXP = /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
+
+  # Validations
+  validates :email,      presence: true, uniqueness: {
+                           message: "You have already expressed your interest."
+                         },
+                         format: {
+                            with: EMAIL_REGEXP,
+                            message: "Invalid email address."
+                         }
+
+end
