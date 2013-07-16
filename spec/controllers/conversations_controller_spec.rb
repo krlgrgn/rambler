9969692adec71665d733dce12dbd8c4ca396f60f
Karl Grogan <krlgrgn@gmail.com>
2013-07-16 21:33:17 +0100

    Comment out the conversation controller spec because I am not sure how I want to handle conversations just yet. Fix users/show view spec

--- spec/controllers/conversations_controller_spec.rb
+++ spec/controllers/conversations_controller_spec.rb
@@ -34,131 +34,133 @@ describe ConversationsController do
     {}
   end
 
-  describe "GET index" do
-    it "assigns all conversations as @conversations" do
-      conversation = Conversation.create! valid_attributes
-      get :index, {}, valid_session
-      assigns(:conversations).should eq([conversation])
-    end
-  end
+  pending "Write some conversation controller tests."
 
-  describe "GET show" do
-    it "assigns the requested conversation as @conversation" do
-      conversation = Conversation.create! valid_attributes
-      get :show, {:id => conversation.to_param}, valid_session
-      assigns(:conversation).should eq(conversation)
-    end
-  end
-
-  describe "GET new" do
-    it "assigns a new conversation as @conversation" do
-      get :new, {}, valid_session
-      assigns(:conversation).should be_a_new(Conversation)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested conversation as @conversation" do
-      conversation = Conversation.create! valid_attributes
-      get :edit, {:id => conversation.to_param}, valid_session
-      assigns(:conversation).should eq(conversation)
-    end
-  end
-
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Conversation" do
-        expect {
-          post :create, {:conversation => valid_attributes}, valid_session
-        }.to change(Conversation, :count).by(1)
-      end
-
-      it "assigns a newly created conversation as @conversation" do
-        post :create, {:conversation => valid_attributes}, valid_session
-        assigns(:conversation).should be_a(Conversation)
-        assigns(:conversation).should be_persisted
-      end
-
-      it "redirects to the created conversation" do
-        post :create, {:conversation => valid_attributes}, valid_session
-        response.should redirect_to(Conversation.last)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved conversation as @conversation" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Conversation.any_instance.stub(:save).and_return(false)
-        post :create, {:conversation => { "message_box_id" => "invalid value" }}, valid_session
-        assigns(:conversation).should be_a_new(Conversation)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Conversation.any_instance.stub(:save).and_return(false)
-        post :create, {:conversation => { "message_box_id" => "invalid value" }}, valid_session
-        response.should render_template("new")
-      end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested conversation" do
-        conversation = Conversation.create! valid_attributes
-        # Assuming there are no other conversations in the database, this
-        # specifies that the Conversation created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        Conversation.any_instance.should_receive(:update_attributes).with({ "message_box_id" => "1" })
-        put :update, {:id => conversation.to_param, :conversation => { "message_box_id" => "1" }}, valid_session
-      end
-
-      it "assigns the requested conversation as @conversation" do
-        conversation = Conversation.create! valid_attributes
-        put :update, {:id => conversation.to_param, :conversation => valid_attributes}, valid_session
-        assigns(:conversation).should eq(conversation)
-      end
-
-      it "redirects to the conversation" do
-        conversation = Conversation.create! valid_attributes
-        put :update, {:id => conversation.to_param, :conversation => valid_attributes}, valid_session
-        response.should redirect_to(conversation)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the conversation as @conversation" do
-        conversation = Conversation.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Conversation.any_instance.stub(:save).and_return(false)
-        put :update, {:id => conversation.to_param, :conversation => { "message_box_id" => "invalid value" }}, valid_session
-        assigns(:conversation).should eq(conversation)
-      end
-
-      it "re-renders the 'edit' template" do
-        conversation = Conversation.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Conversation.any_instance.stub(:save).and_return(false)
-        put :update, {:id => conversation.to_param, :conversation => { "message_box_id" => "invalid value" }}, valid_session
-        response.should render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested conversation" do
-      conversation = Conversation.create! valid_attributes
-      expect {
-        delete :destroy, {:id => conversation.to_param}, valid_session
-      }.to change(Conversation, :count).by(-1)
-    end
-
-    it "redirects to the conversations list" do
-      conversation = Conversation.create! valid_attributes
-      delete :destroy, {:id => conversation.to_param}, valid_session
-      response.should redirect_to(conversations_url)
-    end
-  end
+#  describe "GET index" do
+#    it "assigns all conversations as @conversations" do
+#      conversation = Conversation.create! valid_attributes
+#      get :index, {}, valid_session
+#      assigns(:conversations).should eq([conversation])
+#    end
+#  end
+#
+#  describe "GET show" do
+#    it "assigns the requested conversation as @conversation" do
+#      conversation = Conversation.create! valid_attributes
+#      get :show, {:id => conversation.to_param}, valid_session
+#      assigns(:conversation).should eq(conversation)
+#    end
+#  end
+#
+#  describe "GET new" do
+#    it "assigns a new conversation as @conversation" do
+#      get :new, {}, valid_session
+#      assigns(:conversation).should be_a_new(Conversation)
+#    end
+#  end
+#
+#  describe "GET edit" do
+#    it "assigns the requested conversation as @conversation" do
+#      conversation = Conversation.create! valid_attributes
+#      get :edit, {:id => conversation.to_param}, valid_session
+#      assigns(:conversation).should eq(conversation)
+#    end
+#  end
+#
+#  describe "POST create" do
+#    describe "with valid params" do
+#      it "creates a new Conversation" do
+#        expect {
+#          post :create, {:conversation => valid_attributes}, valid_session
+#        }.to change(Conversation, :count).by(1)
+#      end
+#
+#      it "assigns a newly created conversation as @conversation" do
+#        post :create, {:conversation => valid_attributes}, valid_session
+#        assigns(:conversation).should be_a(Conversation)
+#        assigns(:conversation).should be_persisted
+#      end
+#
+#      it "redirects to the created conversation" do
+#        post :create, {:conversation => valid_attributes}, valid_session
+#        response.should redirect_to(Conversation.last)
+#      end
+#    end
+#
+#    describe "with invalid params" do
+#      it "assigns a newly created but unsaved conversation as @conversation" do
+#        # Trigger the behavior that occurs when invalid params are submitted
+#        Conversation.any_instance.stub(:save).and_return(false)
+#        post :create, {:conversation => { "message_box_id" => "invalid value" }}, valid_session
+#        assigns(:conversation).should be_a_new(Conversation)
+#      end
+#
+#      it "re-renders the 'new' template" do
+#        # Trigger the behavior that occurs when invalid params are submitted
+#        Conversation.any_instance.stub(:save).and_return(false)
+#        post :create, {:conversation => { "message_box_id" => "invalid value" }}, valid_session
+#        response.should render_template("new")
+#      end
+#    end
+#  end
+#
+#  describe "PUT update" do
+#    describe "with valid params" do
+#      it "updates the requested conversation" do
+#        conversation = Conversation.create! valid_attributes
+#        # Assuming there are no other conversations in the database, this
+#        # specifies that the Conversation created on the previous line
+#        # receives the :update_attributes message with whatever params are
+#        # submitted in the request.
+#        Conversation.any_instance.should_receive(:update_attributes).with({ "message_box_id" => "1" })
+#        put :update, {:id => conversation.to_param, :conversation => { "message_box_id" => "1" }}, valid_session
+#      end
+#
+#      it "assigns the requested conversation as @conversation" do
+#        conversation = Conversation.create! valid_attributes
+#        put :update, {:id => conversation.to_param, :conversation => valid_attributes}, valid_session
+#        assigns(:conversation).should eq(conversation)
+#      end
+#
+#      it "redirects to the conversation" do
+#        conversation = Conversation.create! valid_attributes
+#        put :update, {:id => conversation.to_param, :conversation => valid_attributes}, valid_session
+#        response.should redirect_to(conversation)
+#      end
+#    end
+#
+#    describe "with invalid params" do
+#      it "assigns the conversation as @conversation" do
+#        conversation = Conversation.create! valid_attributes
+#        # Trigger the behavior that occurs when invalid params are submitted
+#        Conversation.any_instance.stub(:save).and_return(false)
+#        put :update, {:id => conversation.to_param, :conversation => { "message_box_id" => "invalid value" }}, valid_session
+#        assigns(:conversation).should eq(conversation)
+#      end
+#
+#      it "re-renders the 'edit' template" do
+#        conversation = Conversation.create! valid_attributes
+#        # Trigger the behavior that occurs when invalid params are submitted
+#        Conversation.any_instance.stub(:save).and_return(false)
+#        put :update, {:id => conversation.to_param, :conversation => { "message_box_id" => "invalid value" }}, valid_session
+#        response.should render_template("edit")
+#      end
+#    end
+#  end
+#
+#  describe "DELETE destroy" do
+#    it "destroys the requested conversation" do
+#      conversation = Conversation.create! valid_attributes
+#      expect {
+#        delete :destroy, {:id => conversation.to_param}, valid_session
+#      }.to change(Conversation, :count).by(-1)
+#    end
+#
+#    it "redirects to the conversations list" do
+#      conversation = Conversation.create! valid_attributes
+#      delete :destroy, {:id => conversation.to_param}, valid_session
+#      response.should redirect_to(conversations_url)
+#    end
+#  end
 
 end
--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "users/show" do
   before(:each) do
     @user = assign(:user, stub_model(User))
+    @message = assign(:message, stub_model(Message))
   end
 
   it "renders attributes in <p>" do
